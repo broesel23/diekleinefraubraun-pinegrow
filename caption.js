a989a5c071956cd25a77f1688a29e94904e4e550
broesel23 <bebi@gmx.de>
2021-08-22 17:09:27 +0200

    JavaScript may useful for captions of slides

---
+++ caption.js
@@ -0,0 +1,13 @@
+$(function() {
+    $('.carousel').carousel();
+    var caption = $('div.item:nth-child(1) .carousel-caption');
+    $('#slide-caption h4').html(caption.html());
+    caption.css('display','none');
+    $(".carousel").on('slide.bs.carousel', function(evt) {
+    
+    
+       var caption = $('div.item:nth-child(' + ($(evt.relatedTarget).index()+1) + ') .carousel-caption');
+       $('#slide-caption h4').html(caption.html());
+       caption.css('display','none');
+    });
+});
