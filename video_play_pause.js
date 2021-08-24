196754374ec59c68f130a4f86146095daafaa117
broesel23 <bebi@gmx.de>
2021-08-24 10:42:30 +0200

    vdieo-integration with pausing slides

---
+++ video_play_pause.js
@@ -0,0 +1,19 @@
+/* this script is not working !
+   use "pause on hover for the slider-element instead.
+ */
+/*
+player.on('mouseout', function() {
+   $('#slider').carousel({
+        pause: false,
+        interval: true
+    });
+});
+
+player.on('mouseover', function(){ 
+  $('#slider').carousel({
+        pause: true,
+        interval: false
+    });
+});
+
+*/
