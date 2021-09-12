79175d01b618bbdc801492aa6158388f53321e82
broesel23 <bebi@gmx.de>
2021-09-12 09:38:38 +0200

    fixed play-pause button stati on clicking arrow-buttons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,25 +6,34 @@ $("#slider").on('slide.bs.carousel', function(evt) {
             $('#caption-'+step).fadeIn();
     });
 
-/* HIER MUSS AUCH DER Zustand von '.btn-customized' passend gesetzt werden. Falls der Button auf btn-pause war, muss passend getoggled werden... Noch genau anschauen!
-*/
+	/* HIER MUSS AUCH DER Zustand von '.btn-customized' passend gesetzt werden. Falls der Button auf btn-pause war, muss passend getoggled werden... Leichte Ungenauigkeit bzgl. Farbe des Pause-Buttons resultiert durch aktives Setzen von Fokus. Ist aber bzgl. des Zustands wenigstens nun konsistent. Damit wird wirklich beim Klicken von Pause angehalten und beim Klicken eines der Pfeil-Buttons werden die Play/Pause-Button Zustände korrekt gesetzt bis auf die zu intensive Farbe des Pause-Buttons - kann man verschmerzen */
+	
+  	set_pause_play_buttons();
+	      
+});
 
 /*
-    if ($('#video.carousel-item').hasClass('active')) {
-                $("#slider").carousel.slide.data-ride('paused');
-        } /* else {
-        $("#slider").carousel.slide.data-ride('carousel'); */        
-});
+    Set status of Play/Pause buttons according to cycle or pause of carousel
+*/
+
+function set_pause_play_buttons(){
+
+    $('.btn-cycle').toggleClass('disabled',true);
+	$('.btn-cycle').blur();
+	$('.btn-pause').toggleClass('disabled',false);
+    $('.btn-pause').focus();
+}
 
 /*
     Stop / Start carousel autoplay
 */
+
 $('.btn-customized').on('click', function(){
  
     if( ! $(this).hasClass('disabled') ) {
  
         if( $(this).hasClass('btn-pause') ) {
-            $('#slider').carousel('pause'); /* .carousel */
+            $('#slider').carousel('pause'); 
         }
         else {
             $('#slider').carousel('cycle');
@@ -33,6 +42,7 @@ $('.btn-customized').on('click', function(){
         $('.btn-pause, .btn-cycle').toggleClass('disabled');
         $(this).blur();
  
-    }
+    } 
+}
  
-});
+);
